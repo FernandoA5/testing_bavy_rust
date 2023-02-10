307e0ade6511e663fbb2b68482da5d3d9e87e71c
Fernando Alcalá <fernandoalcalacasas@hotmail.com>
2023-02-10 14:56:09 -0600

    ventana (no funciona)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use bevy::prelude::*;
+use heron::prelude::*;
+use rand::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::build()
+        .insert_resource(WindowDescriptor{
+            title: "Pong".to_string(),
+            width: 640.0,
+            height: 480.0,
+            vsync: true,
+            ..Default::default()
+        })
+        .insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)))
+        .add_plugin(PhysicsPlugin::default())
+        .add_plugins(DefaultPlugins)
+        .run();
 }
